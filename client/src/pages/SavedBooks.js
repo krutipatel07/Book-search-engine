3a42b86b5ec1304e78dc90fd9b7ea3f0d080c64e
Kruti Patel <kruti.patel0709@gmail.com>
2021-09-11 20:00:02 -0400

    when enter a valid email address and create a password and click on the signup button, user account is created and user logged in to the site

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -44,7 +44,7 @@ const SavedBooks = () => {
             console.error(err);
         }
     };
-    
+
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
@@ -101,40 +101,43 @@ const SavedBooks = () => {
   if (!userDataLength) {
     return <h2>LOADING...</h2>;
   }
-
-  return (
-    <>
-      <Jumbotron fluid className='text-light bg-dark'>
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </Jumbotron>
-      <Container>
-        <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <CardColumns>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns>
-      </Container>
-    </>
-  );
-};
+    return (
+        <>
+            <Jumbotron fluid className='text-light bg-dark'>
+                <Container>
+                    <h1>Viewing saved books!</h1>
+                </Container>
+            </Jumbotron>
+            <Container>
+                <h2>
+                    {user.savedBooks.length
+                        ? `Viewing ${user.savedBooks.length} saved ${user.savedBooks.length === 1 ? 'book' : 'books'}:`
+                        : 'You have no saved books!'}
+                </h2>
+                <CardColumns>
+                    {user.savedBooks.map((book) => {
+                        return (
+                            <Card key={book.bookId} border='dark'>
+                                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                                <Card.Body>
+                                    <Card.Title>
+                                        <a href={book.link} target="_blank">
+                                            {book.title}
+                                        </a>
+                                        </Card.Title>
+                                    <p className='small'>Authors: {book.authors}</p>
+                                    <Card.Text>{book.description}</Card.Text>
+                                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                                        Delete this Book!
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        );
+                    })}
+                </CardColumns>
+            </Container>
+        </>
+    );
+                  }
 
 export default SavedBooks;
